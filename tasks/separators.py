formula simplification fixes

tasks/separators.py
@@ -120,7 +120,7 @@ def generateLocallyClosedBiSeparator(net: Net, U, msrc, mtgt):
         clause = Clause([Atom(a, a)])
         for t in net.transitions:
             clause.syndrome[t.name] = clause
-        return Formula([clause])
+        return Formula(net, [clause])
     
     # X solver initialization
     X = Solver()
@@ -156,7 +156,7 @@ def generateLocallyClosedBiSeparator(net: Net, U, msrc, mtgt):
         clause = Clause([Atom(yempty, yempty)])
         for t in net.transitions:
             clause.syndrome[t.name] = clause
-        return Formula([clause])
+        return Formula(net, [clause])
 
     #Case X none empty
     else:
@@ -186,7 +186,7 @@ def generateLocallyClosedBiSeparator(net: Net, U, msrc, mtgt):
             clauses_case1.append(clause)
             clauses_Cu_UDiffUp[t.name] = clause
             if np.dot(yt,msrc)>np.dot(yt,mtgt):
-                return Formula([clause])
+                return Formula(net, [clause])
 
         # Compute largest siphon and trap
         vectQ = largestSiphon(net, Up, msrc)
@@ -238,7 +238,7 @@ def generateLocallyClosedBiSeparator(net: Net, U, msrc, mtgt):
 
         # Formula concatenation
         clauses = clauses_case1 + clauses_case2 + clauses_case3
-        bisep = Formula(clauses)
+        bisep = Formula(net, clauses)
         return bisep
 
 

tasks/simplify.py
@@ -4,19 +4,56 @@ from z3 import *
 from objects.Net import Net, Transition, Place
 from objects.Formula import Formula, Clause, Atom
 
-def checkClauseUsefulness(form: Formula, clause: Clause):
-    pass
+def checkClauseUselessness(form: Formula, i):
+    # form = A or B with A = C1 and B = C2 or C3
+    # If (A and not-B) unsatisfiable, then A is useless
+    if form.size <= 1:
+        return False
+
+    A = form.clauses[i]
+    B = form.clauses[:i]+form.clauses[i+1:]
+
+    s = Solver()
+    n = form.net.p
+    x = np.array([Real("x%i" % i) for i in range(n)])
+    xp = np.array([Real("xp%i" % i) for i in range(n)])
+
+    andA = []
+    for atom in A.atoms:
+        if atom.strict:
+            andA.append(np.dot(atom.a,x)<np.dot(atom.ap,xp))
+        else:
+            andA.append(np.dot(atom.a,x)<=np.dot(atom.ap,xp))
+    AZ3 = And(andA)
+
+    orB = []
+    for clause in B:
+        andB = []
+        for atom in clause.atoms:
+            if atom.strict:
+                andB.append(np.dot(atom.a,x)<np.dot(atom.ap,xp))
+            else:
+                andB.append(np.dot(atom.a,x)<=np.dot(atom.ap,xp))
+        orB.append(And(andB))
+    BZ3 = Or(orB)
+
+    cstrt = And(AZ3, Not(BZ3))
+    s.add(simplify(cstrt))
+    return s.check() == unsat
+
 
 def simplifySeparator(sep: Formula):
     simplified = False
     currentSep = sep
     while not simplified:
         clauseRemoved = False
-        for clause in sep.clauses:
-            if not checkClauseUsefulness(currentSep, clause):
-                currentSep = currentSep
-                # remove clause from currentSep
+        for i in range(len(currentSep.clauses)):
+            clause = currentSep.clauses[i]
+            if checkClauseUselessness(currentSep, i):
+                currentSep.clauses.pop(i) # remove clause from currentSep
+                currentSep.size -= 1
                 clauseRemoved = True
                 break
         if not clauseRemoved: 
             simplified = True
+    return currentSep